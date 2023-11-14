7f8517ed7462991719b969cdca74f61145761bbf
june0929 <qk1413@naver.com>
2023-11-14 23:23:50 +0900

    fix: Multiple tab creation issue

--- src/extension.ts
+++ src/extension.ts
@@ -3,35 +3,48 @@ import { GameProvider } from "./game-provider";
 import metaData, { MetaData } from "./game/meta-data";
 import getHtmlContent from "./util/get-html-content";
 
+//WebView 관리 맵(game.id, panel instance)
+const openWebviews = new Map<string, vscode.WebviewPanel>();
+
 export function activate(context: vscode.ExtensionContext) {
   // GameProvider 인스턴스 생성
   const gameProvider = new GameProvider();
   // 'gameExplorer' 뷰 컨테이너에 대한 TreeDataProvider로 gameProvider를 등록
   vscode.window.registerTreeDataProvider("gameExplorer", gameProvider);
-
   // 'gameExtension.openGame' 커맨드를 등록하고, 실행될 때 createGameWebview 함수를 호출
   const openGamePanel = (context: vscode.ExtensionContext, game: MetaData) => {
-    const panel = vscode.window.createWebviewPanel(
-      game.id, // 웹뷰의 유형
-      game.title, // 사용자에게 표시될 패널 제목
-      vscode.ViewColumn.Active, // 웹뷰 패널을 표시할 에디터 열
-      {
-        enableScripts: true, // 스크립트 활성화
-        localResourceRoots: [
-          vscode.Uri.file(
-            vscode.Uri.joinPath(context.extensionUri, "src/game", game.id)
-              .fsPath
-          ),
-          vscode.Uri.file(
-            vscode.Uri.joinPath(context.extensionUri, "resource/game", game.id)
-              .fsPath
-          ),
-        ],
-      }
-    );
+    if (openWebviews.has(game.id)) {
+      const webViewPanel = openWebviews.get(game.id);
+      webViewPanel?.reveal();
+    } else {
+      const panel = vscode.window.createWebviewPanel(
+        game.id, // 웹뷰의 유형
+        game.title, // 사용자에게 표시될 패널 제목
+        vscode.ViewColumn.Active, // 웹뷰 패널을 표시할 에디터 열
+        {
+          retainContextWhenHidden: true,
+          enableScripts: true, // 스크립트 활성화
+          localResourceRoots: [
+            vscode.Uri.file(
+              vscode.Uri.joinPath(context.extensionUri, "src/game", game.id)
+                .fsPath
+            ),
+            vscode.Uri.file(
+              vscode.Uri.joinPath(
+                context.extensionUri,
+                "resource/game",
+                game.id
+              ).fsPath
+            ),
+          ],
+        }
+      );
+      panel.webview.html = getHtmlContent(context, panel.webview, game);
 
-    panel.webview.html = getHtmlContent(context, panel.webview, game);
-    return panel;
+      openWebviews.set(game.id, panel);
+
+      panel.onDidDispose(() => openWebviews.delete(game.id));
+    }
   };
 
   let openGameCommand = vscode.commands.registerCommand(
