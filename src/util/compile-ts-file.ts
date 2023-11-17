2b9c421ce3e37ce5d67814f896b8934ea94c221f
smshack <5432tat@naver.com>
2023-11-17 21:46:30 +0900

    Add game webview test code by returning separate metadata to utility with createWebviewPanel function

--- src/util/compile-ts-file.ts
+++ src/util/compile-ts-file.ts
@@ -1,15 +1,15 @@
-import * as path from "path";
-import * as ts from "typescript";
-import * as fs from "fs";
+import * as path from 'path';
+import * as ts from 'typescript';
+import * as fs from 'fs';
 
 function compileTypeScript(filePath: string): string {
-  const tsCode = fs.readFileSync(filePath, "utf8");
+  const tsCode = fs.readFileSync(filePath, 'utf8');
   const jsCode = ts.transpileModule(tsCode, {
     compilerOptions: { module: ts.ModuleKind.CommonJS },
   }).outputText;
   const compiledFilePath = path.join(
     path.dirname(filePath),
-    path.basename(filePath, ".ts") + ".js"
+    path.basename(filePath, '.ts') + '.js'
   );
   fs.writeFileSync(compiledFilePath, jsCode);
   return compiledFilePath;
--- src/util/get-html-content.ts
+++ src/util/get-html-content.ts
@@ -1,7 +1,7 @@
-import * as vscode from "vscode";
-import * as path from "path";
-import * as fs from "fs";
-import { MetaData } from "../game/meta-data";
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import { MetaData } from '../game/meta-data';
 
 function getHtmlContent(
   context: vscode.ExtensionContext,
@@ -9,16 +9,16 @@ function getHtmlContent(
   game: MetaData
 ): string {
   let htmlContent = fs.readFileSync(
-    path.join(context.extensionPath, "resource/game", game.id, "index.html"),
-    "utf8"
+    path.join(context.extensionPath, 'resource/game', game.id, 'index.html'),
+    'utf8'
   );
 
   // 리소스 경로 변환 (스타일시트, 스크립트, 이미지 등)
   htmlContent = htmlContent.replace(
     /(href|src|data)="(?!https)([^"]*)"/g,
-    (_: any, type: "href" | "src" | "data", extraPath: string) => {
+    (_: any, type: 'href' | 'src' | 'data', extraPath: string) => {
       const onDiskPath = vscode.Uri.file(
-        path.join(context.extensionPath, "resource/game", game.id, extraPath)
+        path.join(context.extensionPath, 'resource/game', game.id, extraPath)
       );
       const webViewPath = webview.asWebviewUri(onDiskPath);
       return `${type}="${webViewPath}"`;
