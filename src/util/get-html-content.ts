7f7c8d9ea66e196aa8e686dc20ca04725703f7a5
june0929 <qk1413@naver.com>
2023-11-19 20:09:43 +0900

    fix: Add send score function in get-html-content class

--- src/util/get-html-content.ts
+++ src/util/get-html-content.ts
@@ -1,7 +1,7 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
-import { MetaData } from '../game/meta-data';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import { MetaData } from "../game/meta-data";
 
 function getHtmlContent(
   context: vscode.ExtensionContext,
@@ -9,16 +9,39 @@ function getHtmlContent(
   game: MetaData
 ): string {
   let htmlContent = fs.readFileSync(
-    path.join(context.extensionPath, 'resource/game', game.id, 'index.html'),
-    'utf8'
+    path.join(context.extensionPath, "resource/game", game.id, "index.html"),
+    "utf8"
   );
 
-  // 리소스 경로 변환 (스타일시트, 스크립트, 이미지 등)
+  const scoreScript = `
+    <script>
+      // 전역 범위에서 VS Code API 인스턴스 획득
+      const vscode = acquireVsCodeApi();
+
+      // 점수 전송 함수
+      function sendScore(player, score) {
+        console.log("player: ", player);
+        console.log("score: ", score);
+        vscode.postMessage({
+          command: 'sendScore',
+          score: score,
+          player: player
+        });
+      }
+    </script>
+  `;
+    
+  htmlContent = htmlContent.replace(
+    /<\/body>/,
+    scoreScript + '</body>'
+  );
+  console.log('htmlContent',htmlContent);
+
   htmlContent = htmlContent.replace(
     /(href|src|data)="(?!https)([^"]*)"/g,
-    (_: any, type: 'href' | 'src' | 'data', extraPath: string) => {
+    (_: any, type: "href" | "src" | "data", extraPath: string) => {
       const onDiskPath = vscode.Uri.file(
-        path.join(context.extensionPath, 'resource/game', game.id, extraPath)
+        path.join(context.extensionPath, "resource/game", game.id, extraPath)
       );
       const webViewPath = webview.asWebviewUri(onDiskPath);
       return `${type}="${webViewPath}"`;
