5362d7a64c1a2507590c4a8e9e584849f040ec7a
june0929 <qk1413@naver.com>
2023-11-19 22:05:31 +0900

    fix: apply prettier

--- src/score-board-provider.ts
+++ src/score-board-provider.ts
@@ -8,7 +8,10 @@ interface Score {
 }
 
 export class ScoreBoardProvider implements vscode.TreeDataProvider<ScoreItem> {
-  constructor(private context: vscode.ExtensionContext, private gameId: string) {}
+  constructor(
+    private context: vscode.ExtensionContext,
+    private gameId: string
+  ) {}
 
   getTreeItem(element: ScoreItem): vscode.TreeItem {
     return element;
@@ -19,21 +22,25 @@ export class ScoreBoardProvider implements vscode.TreeDataProvider<ScoreItem> {
   }
 
   private getScores(): Thenable<ScoreItem[]> {
-    const scoresPath = path.join(this.context.globalStorageUri.fsPath, `${this.gameId}-scores.json`);
+    const scoresPath = path.join(
+      this.context.globalStorageUri.fsPath,
+      `${this.gameId}-scores.json`
+    );
     if (fs.existsSync(scoresPath)) {
-      const scoresData = JSON.parse(fs.readFileSync(scoresPath, 'utf8')) as Score[];
+      const scoresData = JSON.parse(
+        fs.readFileSync(scoresPath, 'utf8')
+      ) as Score[];
       const sortedScores = scoresData.sort((a, b) => b.score - a.score);
-      return Promise.resolve(sortedScores.map(score => new ScoreItem(score.player, score.score)));
+      return Promise.resolve(
+        sortedScores.map(score => new ScoreItem(score.player, score.score))
+      );
     }
     return Promise.resolve([]);
   }
 }
 
 class ScoreItem extends vscode.TreeItem {
-  constructor(
-    public readonly player: string,
-    public readonly score: number,
-  ) {
+  constructor(public readonly player: string, public readonly score: number) {
     super(`${player}: ${score}점`);
   }
 }
