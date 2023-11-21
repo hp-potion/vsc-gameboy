9cc3cdecbd4952ad00f72d49c62c57d994e2f96c
junman95 <junman95@naver.com>
2023-11-21 23:26:22 +0900

    chore: config vscode-test.js and jest.config.js to seperate file path npm commands seperated from test -> test, test-util, test-extension test command test combines other two test commands

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // other Jest configuration options...
+
+  testMatch: [
+    '**/__tests__/**/*.+(js|jsx|ts|tsx)', // Include .jsx, .js, .tsx, .ts files in __tests__ directories
+    '**/?(*.)+(spec).+(js|jsx|ts|tsx)', // Include .jsx, .js, .tsx, .ts files with spec file only in the filename
+  ],
+};
